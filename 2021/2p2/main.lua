6585e16864a0e53be4fa1a625b273195d80bdf9f
Fox <cosmo8072@gmail.com>
2021-12-02 13:14:03 +0800

    Day 2 solutions

--- 2021/2p2/main.lua
+++ 2021/2p2/main.lua
@@ -1,6 +1,7 @@
 #!/usr/bin/env lua5.4
 
 local x, y = 0, 0
+local aim = 0
 
 for line in io.lines() do
   local op = line:match("^[a-z]+")
@@ -8,12 +9,14 @@ for line in io.lines() do
   
   if op == "forward" then
     x = x + changes
+    y = y + aim * changes
   elseif op == "up" then
-    y = y - changes
+    aim = aim - changes
   elseif op == "down" then
-    y = y + changes
+    aim = aim + changes
   end
 end
+
 print(x * y)
 
 
